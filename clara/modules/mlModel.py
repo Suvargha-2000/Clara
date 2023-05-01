44ddfeb1494ac9e450ed48dd4136b5c8a8ce8220
Suvargha Ghosh Dastidar <suvargha2000@gmail.com>
2023-05-02 00:21:28 +0530

    creating models

--- clara/modules/mlModel.py
+++ clara/modules/mlModel.py
@@ -1,47 +1,57 @@
 import nltk
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import LabelEncoder
-from sklearn.linear_model import LogisticRegression
 import json
-import numpy as np
 import pandas as pd 
 from sklearn.svm import SVC 
-from sklearn.ensemble import RandomForestClassifier
+import pickle , os
+from sklearn.model_selection import GridSearchCV
 
-sno = nltk.stem.SnowballStemmer('english')
-df = pd.DataFrame()
+def createModel() :
+    sno = nltk.stem.SnowballStemmer('english')
+    df = pd.DataFrame()
 
-with open("clara/training_data/training.json") as file:
-    data = json.load(file)
+    with open("clara/training_data/training.json") as file:
+        data = json.load(file)
 
-data = data["intents"]
-sentence_arr = []
-labels = []
+    data = data["intents"]
+    sentence_arr = []
+    labels = []
 
-for intent in data:
-    for sentence in intent["patterns"]:
-        labels.append(intent["tag"])
-        sentence_arr.append((" ".join([sno.stem(x.lower()) for x in sentence.split(" ")])))
+    for intent in data:
+        for sentence in intent["patterns"]:
+            labels.append(intent["tag"])
+            sentence_arr.append((" ".join([sno.stem(x.lower()) for x in sentence.split(" ")])))
 
-df = pd.DataFrame({"text":sentence_arr  , "labels" : labels})
-le = LabelEncoder()
-transformer = TfidfVectorizer()
-Y = le.fit_transform(labels)
-X_train = transformer.fit_transform(df["text"])
+    df = pd.DataFrame({"text":sentence_arr  , "labels" : labels})
+    le = LabelEncoder()
+    transformer = TfidfVectorizer()
+    Y = le.fit_transform(labels)
+    X_train = transformer.fit_transform(df["text"])
 
-clf2 = LogisticRegression(C=10 , multi_class="multinomial")
-clf2.fit(X_train , Y)
+    if not os.path.isfile("data/bestParam.pkl") :
+        param_grid = {'C': [0.01, 0.1, 1, 10, 100],
+                    'kernel': ['linear', 'poly', 'rbf', 'sigmoid']}
 
-sentence = "log off for today"
+        grid = GridSearchCV(SVC(), param_grid, cv=5)
+        grid.fit(X_train, Y)
 
-result = clf2.predict(transformer.transform([sentence]))
-print(le.inverse_transform(result))
+        print("Best hyperparameters: ", grid.best_params_)
+        pickle.dump(grid.best_params_ , open("data/bestParam.pkl" , "wb"))
+        grid = grid.best_params_
+
+    else : 
+        grid = pickle.load(open("data/bestParam.pkl" , "rb"))
+
+    print(grid)
+
+    clf = SVC(C = grid["C"] , kernel=grid["kernel"])  
+    clf.fit(X_train , Y)
+    pickle.dump((clf , le , transformer) , open("data/svc.pkl" , "wb"))
 
 
-clf = SVC(kernel='linear')  
-clf.fit(X_train , Y)
-result = clf.predict(transformer.transform([sentence]))
-print(le.inverse_transform(result))
+def askAi(sentence):
+    clf , le , transformer= pickle.load(open("data/svc.pkl" , "rb"))
 
-
-
+    result = clf.predict(transformer.transform([sentence]))
+    return le.inverse_transform(result)
